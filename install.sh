3bcbaf023524dc78acf25707d5fbbb0d69fd67f8
Darbit Dev <info@darbit.ru>
2024-06-28 01:53:18 +0500

    fixes

--- install.sh
+++ install.sh
@@ -1,95 +1,163 @@
 #!/bin/sh
 set -e
 
+REPO_PATH=https://github.com/darbit-ru/bitrixdock.git
 GROUP_NAME=www-data
 INSTALL_COMMAND=apt-get
 if [[ -n "$(command -v yum)" ]]; then
   INSTALL_COMMAND=yum
 fi
 
-#checking is www-data group exist
-echo -e "\e[33mChecking GIT \e[39m"
+# checking is www-data group exist
+echo -e "\e[33mChecking user group www-data \e[39m"
 if [[ $(getent group $GROUP_NAME) ]]
 then
   echo -e "\e[32m    Group exist \e[39m"
 else
-  echo -e "\e[31m    Group not exist, adding group - $GROUP_NAME \e[39m" && groupadd $GROUP_NAME
+  echo -e "\e[31m    Group not exist, adding group - $GROUP_NAME \e[39m" && groupadd $GROUP_NAME > /dev/null 2>&1
 fi
 
-
-#checking is git installed
+# checking is git installed
 echo -e "\e[33mChecking GIT \e[39m"
 if hash git > /dev/null 2>&1
 then
   echo -e "\e[32m    GIT installed \e[39m"
 else
-  echo -e "\e[31m    GIT not installed, install started \e[39m" && $INSTALL_COMMAND install -y git
+  echo -e "\e[31m    GIT not installed, install started \e[39m" && $INSTALL_COMMAND install -y git > /dev/null 2>&1
 fi
 
-#checking is docker installed
+# checking is docker installed
 echo -e "\e[33mChecking DOCKER \e[39m"
 if hash docker > /dev/null 2>&1
 then
   echo -e "\e[32m    DOCKER installed \e[39m"
 else
-  echo -e "\e[31m    DOCKER not installed, install started \e[39m" && cd /usr/local/src && wget -qO- https://get.docker.com/ | sh
+  echo -e "\e[31m    DOCKER not installed, install started \e[39m" && cd /usr/local/src && wget -qO- https://get.docker.com/ | sh > /dev/null 2>&1
 fi
 
-#checking is installed docker-compose
+# checking is installed docker-compose
 echo -e "\e[33mChecking DOCKER-COMPOSE \e[39m"
 if hash docker-compose > /dev/null 2>&1
 then
   echo -e "\e[32m    DOCKER-COMPOSE installed \e[39m"
 else
-  echo -e "\e[31m    DOCKER-COMPOSE not installed, install started \e[39m" && curl -L "https://github.com/docker/compose/releases/download/1.25.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose && chmod +x /usr/local/bin/docker-compose && source ~/.bashrc
+  echo -e "\e[31m    DOCKER-COMPOSE not installed, install started \e[39m" && curl -L "https://github.com/docker/compose/releases/download/1.25.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose && chmod +x /usr/local/bin/docker-compose && source ~/.bashrc > /dev/null 2>&1
 fi
 
-#show message that all required packets installed
+# show message that all required packets installed
 echo -e "\n\e[32mAll required packets installed \e[39m\n\n"
 
-echo -e "\e[33mSet work folder path (left empty for use default path. Default path = /var/www/)\e[39m"
+# reading work folder path
+echo -e "\e[33mSet work folder path (left empty for use default path. Default path = /var/www)\e[39m"
 read WORK_PATH
 
+# if work folder is empty, setting default value
 if [[ "$WORK_PATH" = "" ]]; then
   WORK_PATH=/var/www
 fi
-
-echo $WORK_PATH
 
 # downloading docker from git source
 DOCKER_FOLDER_PATH=$WORK_PATH/bitrixdock
 if [ ! -d "$DOCKER_FOLDER_PATH" ]
 then
-  mkdir $DOCKER_FOLDER_PATH
+  #creating work folders if they are not exist
+  mkdir -p $DOCKER_FOLDER_PATH
 
-  echo -e "\e[33mDocker containers is not installed. Installation starting... \e[39m\n"
+  echo -e "\n\n\e[33mDocker containers is not installed. Installation starting... \e[39m\n"
 
+  # downloading files from repo
+  echo -e "\e[33mCloning repo to local... \e[39m"
   cd $WORK_PATH && \
-  git clone https://github.com/darbit-ru/bitrixdock.git && \
-  cd /var/ && chmod -R 775 www/ && chown -R root:www-data www/ && \
+  git clone $REPO_PATH > /dev/null 2>&1 && \
+  cd $(dirname $WORK_PATH) && chmod -R 775 $(basename $WORK_PATH) && chown -R root:www-data $(basename $WORK_PATH) && \
   cd $DOCKER_FOLDER_PATH
+  echo -e "\e[32m    Done \e[39m\n"
+
+  # copy .env file from template file
+  echo -e "\e[33mCopy environment setting file and starting configuration \e[39m"
+  cp -f .env_template .env && \
+  echo -e "\e[32m    Done \e[39m\n"
+
+  # chosing PHP version
+  echo -e "\e[33mSelect PHP version [7.4, 8.0, 8.1, 8.2, 8.3]: \e[39m"
+  read PHP_VERSION
+  until [[ $PHP_VERSION != "7.4" || $PHP_VERSION != "8.0" || $PHP_VERSION != "8.1" || $PHP_VERSION != "8.2" || $PHP_VERSION != "8.3" ]]
+  do
+      echo -e "\e[33mSelect PHP version [7.4, 8.0, 8.1, 8.2, 8.3]: \e[39m"
+      read PHP_VERSION
+  done
+  echo -e "\n"
+
+  # select php version
+  if [[ $PHP_VERSION == "7.4" ]]; then
+    SELECTED_PHP_VERSION=php74
+  elif [[ $PHP_VERSION == "8.0" ]]; then
+    SELECTED_PHP_VERSION=php80
+  elif [[ $PHP_VERSION == "8.1" ]]; then
+    SELECTED_PHP_VERSION=php81
+  elif [[ $PHP_VERSION == "8.2" ]]; then
+    SELECTED_PHP_VERSION=php82
+  elif [[ $PHP_VERSION == "8.3" ]]; then
+      SELECTED_PHP_VERSION=php83
+  fi
+  sed -i "s/#PHP_VERSION#/$SELECTED_PHP_VERSION/g" $DOCKER_FOLDER_PATH/.env
+
+  # set database root password
+  echo -e "\e[33mSet MYSQL database ROOT PASSWORD: \e[39m"
+  read MYSQL_DATABASE_ROOT_PASSWORD
+  until [[ ! -z "$MYSQL_DATABASE_ROOT_PASSWORD" ]]
+  do
+    echo -e "\e[33mSet MYSQL database ROOT PASSWORD: \e[39m"
+    read MYSQL_DATABASE_ROOT_PASSWORD
+  done
+  sed -i "s/#DATABASE_ROOT_PASSWORD#/$MYSQL_DATABASE_ROOT_PASSWORD/g" $DOCKER_FOLDER_PATH/.env
+
+  # checking site name domain
+  echo -e "\e[33mEnter site name (websitename.domain | example: mail.ru): \e[39m"
+  read SITE_NAME
+  domainRegex="(^([a-zA-Z0-9](([a-zA-Z0-9-]){0,61}[a-zA-Z0-9])?\.)+[a-zA-Z]{0,10}$)"
+  until [[ $SITE_NAME =~ $domainRegex ]]
+  do
+    echo -e "\e[33mEnter site name (websitename.domain | example: mail.ru): \e[39m"
+    read SITE_NAME
+  done
+
+  # checking site installation type
+  echo -e "\e[33mSite installation type? (C - clear install bitrixsetup.php / R - restore from backup): \e[39m"
+  read INSTALLATION_TYPE
+  until [[ $INSTALLATION_TYPE == [CR] ]]
+  do
+    echo -e "\e[33mSite installation type? (C - clear install bitrixsetup.php / R - restore from backup): \e[39m"
+    read INSTALLATION_TYPE
+  done
+
+  # checking site installation type
+  echo -e "\e[33mDo you want install SSL from letsencrypt? (Y/N): \e[39m"
+  read SSL_INSTALL_ACTION
+  until [[ $SSL_INSTALL_ACTION != "Y" || $SSL_INSTALL_ACTION != "N" ]]
+  do
+    echo -e "\e[33mDo you want install SSL from letsencrypt? (Y/N): \e[39m"
+    read SSL_INSTALL_ACTION
+  done
+
+  # creating website folder
+  WEBSITE_FILES_PATH=$WORK_PATH/bitrix
+  echo -e "\e[33mCreating website folder \e[39m"
+  mkdir -p $WEBSITE_FILES_PATH && \
+  cd $WEBSITE_FILES_PATH && \
+  if [[ $INSTALLATION_TYPE == "C" ]]; then wget http://www.1c-bitrix.ru/download/scripts/bitrixsetup.php > /dev/null 2>&1; elif [[ $INSTALLATION_TYPE == "R" ]]; then wget http://www.1c-bitrix.ru/download/scripts/restore.php > /dev/null 2>&1; fi && \
+  cd $(dirname $WORK_PATH) && chmod -R 775 $(basename $WORK_PATH) && chown -R root:www-data $(basename $WORK_PATH)
+  echo -e "\e[32m    Done \e[39m\n"
+
+  echo -e "\n\e[33mConfiguring NGINX conf file \e[39m"
+  cp -f $DOCKER_FOLDER_PATH/nginx/conf/default.conf_template $DOCKER_FOLDER_PATH/nginx/conf/conf.d/$SITE_NAME.conf && \
+  sed -i "s/#SITE_NAME#/$SITE_NAME/g" $DOCKER_FOLDER_PATH/nginx/conf/conf.d/$SITE_NAME.conf && \
+  sed -i "s|#SITE_PATH#|$WEBSITE_FILES_PATH|g" $DOCKER_FOLDER_PATH/nginx/conf/conf.d/$SITE_NAME.conf && \
+  echo -e "\e[32m    Done \e[39m\n"
+
+  cd $DOCKER_FOLDER_PATH
+  echo -e "\n\e[32mStarting DOCKER containers \e[39m\n"
+  docker-compose up -d > /dev/null 2>&1
 else
   echo -e "\e[33mBitrixDock is installed. Clear all and remove all containers to reinstall\e[39m"
 fi
-
-#
-#
-#
-#
-#
-#echo "Create folder struct"
-#mkdir -p /var/www/bitrix && \
-#cd /var/www && \
-#rm -f /var/www/bitrix/bitrixsetup.php && \
-#curl -fsSL https://www.1c-bitrix.ru/download/scripts/bitrixsetup.php -o /var/www/bitrix/bitrixsetup.php && \
-#rm -rf /var/www/bitrixdock && \
-#git clone --depth=1 https://github.com/bitrixdock/bitrixdock.git && \
-#chmod -R 775 /var/www/bitrix && chown -R root:www-data /var/www/bitrix && \
-#
-#echo "Config"
-#cp -f /var/www/bitrixdock/.env_template /var/www/bitrixdock/.env
-#sed -i 's/SITE_PATH=.\/www/SITE_PATH=\/var\/www\/bitrix/' /var/www/bitrixdock/.env
-#
-#echo "Run"
-#docker compose -p bitrixdock down
-#docker compose -f /var/www/bitrixdock/docker-compose.yml -p bitrixdock up -d
